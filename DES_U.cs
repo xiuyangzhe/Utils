550d39aad767275ec62b7dd109714f9bbb425e03
Rocky Zhou <xiuyangzhe@live.com>
2018-04-26 11:02:16 +0800

    add DESEncrypt functions

---
+++ DES_U.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Zsq.Utils
+{
+    public partial class DES_U
+    {
+        /// <summary>
+        /// DES加密方法
+        /// </summary>
+        /// <param name="text">明文</param>
+        /// <param name="sKey">密钥</param>
+        /// <returns>加密后的密文</returns>
+        public static string DESEncrypt(string text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray;
+            inputByteArray = Encoding.Default.GetBytes(text);
+            var key = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            var iv = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(iv);
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            ms.Dispose();
+            cs.Dispose();
+            return ret.ToString();
+
+        }
+        /// <summary>
+        /// DES解密方法
+        /// </summary>
+        /// <param name="text">密文</param>
+        /// <param name="sKey">密钥</param>
+        /// <returns>解密后的明文</returns>
+        public static string DESDecrypt(string text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            int len;
+            len = text.Length / 2;
+            byte[] inputByteArray = new byte[len];
+            int x, i;
+            for (x = 0; x < len; x++)
+            {
+                i = Convert.ToInt32(text.Substring(x * 2, 2), 16);
+                inputByteArray[x] = (byte)i;
+            }
+            try
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+                des.IV = ASCIIEncoding.ASCII.GetBytes(System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(sKey, "md5").Substring(0, 8));
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                cs.FlushFinalBlock();
+                string estring = Encoding.Default.GetString(ms.ToArray());
+                ms.Dispose();
+                cs.Dispose();
+                return estring;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
